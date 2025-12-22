d9bf63c2d2eaeab912cfeb233c16ba00f68ed207
acemavrick <116685696+acemavrick@users.noreply.github.com>
2025-12-22 13:30:13 -0600

    Initial Commit

---
+++ N-Body/N_BodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  N_BodyApp.swift
+//  N-Body
+//
+//  Created by acemavrick on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct N_BodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
